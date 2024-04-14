4bac9d800a8a3166fcce983ca1cbeae44f817608
rami-dv <rami.dv99@gmail.com>
2024-04-14 15:52:38 -0300

    minimum height on layout

--- src/components/mdx-layout.tsx
+++ src/components/mdx-layout.tsx
@@ -2,6 +2,6 @@ import { PropsWithChildren } from "react";
 
 export default function MDXLayout({ children }: PropsWithChildren) {
   return (
-    <main className="sm:mx-auto max-w-6xl mx-2 my-2 sm:my-4">{children}</main>
+    <main className="sm:mx-auto max-w-6xl mx-2 my-2 sm:my-4 min-h-96">{children}</main>
   );
 }
