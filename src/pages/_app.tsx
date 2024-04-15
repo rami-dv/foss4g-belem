f95818bb1df53f17047a52da926ad4aebce7e269
rami-dv <rami.dv99@gmail.com>
2024-04-14 21:33:26 -0300

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import PageBackground from "@/images/page-background.jpg";
-import FooterBackground from "@/images/footer-background.jpg";
+import Head from "next/head"
+import Favicon from "@/images/favicon.png";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-full">
+      <Head>
+        <link
+          rel="icon"
+          href={Favicon.src}
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </Head>
       <Header />
       <div className="relative z-20">
         <Component {...pageProps} />
