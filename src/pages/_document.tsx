fa3233a2f3759984870464407115e8f9d2f791ec
rami-dv <rami.dv99@gmail.com>
2024-04-29 15:06:54 -0300

    html set lang tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+  render() {
+    const lang = this.props.__NEXT_DATA__.page.slice(1).split("/")[0];
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+      <Html lang={["en", "es", "pt"].includes(lang) ? lang : "en"}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
