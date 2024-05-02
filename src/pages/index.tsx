d9b86a50115d143b766963e8e98b5870cf198a20
rami-dv <rami.dv99@gmail.com>
2024-05-01 22:03:25 -0300

    explicitly export root index metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { metadata } from "@/pages/en/index";
-
-export { metadata } from "@/pages/en/index";
-
-export default function IndexPage() {
-  return <></>;
-}
+export const metadata = {
+  title: "FOSS4G 2024 Belém Brasil",
+  description:
+    "Homepage for the FOSS4G 2024 Conference held in Belém, Pará, Brasil on 02-08 December 2024.",
+};
 
 export async function getStaticProps() {
   return {
