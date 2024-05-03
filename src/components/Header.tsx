9dd05a58101c4c8099447c3992f12d8a44c53ee5
Felipe Sodré <felipe.b4rros@gmail.com>
2024-05-03 19:50:51 -0300

    add callfor papers menu and en, pt, es pages for general and academic track. workshop still missing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,6 +58,31 @@ const menuItems = [
       },
     ],
   },
+    {
+    "label:en": "Call for papers",
+    "label:es": "Call for papers",
+    "label:pt": "Call for papers",
+    links: [
+      {
+        "label:en": "General program",
+        "label:es": "General program",
+        "label:pt": "General program",
+        href: "/general-cfp",
+      },
+      {
+        "label:en": "Workshop program",
+        "label:es": "Workshop program",
+        "label:pt": "Workshop program",
+        href: "/workshop-cfp",
+      },
+      {
+        "label:en": "Academic program",
+        "label:es": "Academic program",
+        "label:pt": "Academic program",
+        href: "/academic-cfp",
+      },
+    ],
+   },
   {
     "label:en": "Map",
     "label:es": "Mapa",
